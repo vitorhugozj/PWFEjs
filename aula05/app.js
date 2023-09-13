3ac0f1281902bbbdb91c9485d1b6113714f514ed
VITOR HUGO NUNES NASCIMENTO <23111767@CFP127ALUNO.LOCAL>
2023-09-13 14:05:19 -0300

    finalizando

---
+++ aula05/app.js
@@ -0,0 +1,84 @@
+'use strict'
+
+const contatos = [
+    {
+        nome: 'Neymar',
+        mensagem: 'Completa o racha amanhã, eu faço a sua',
+        notificacoes: '3',
+        tempo: 'Há 44 minutos',
+        foto: 'neymar.png',
+    },
+    {
+        nome: 'Antony',
+        mensagem: 'Champions hoje, joga de lateral pra nós fera? ',
+        notificacoes: '1',
+        tempo: 'Há 2 horas',
+        foto: 'antony.png',
+    },
+    {
+        nome: 'Papai Cris',
+        mensagem: 'Esse Messi é um merda',
+        notificacoes: '+9',
+        tempo: 'Há 6 dias',
+        foto: 'cr7.png',
+    },
+    {
+        nome: 'Messi',
+        mensagem: 'Cristiano é um invejoso',
+        notificacoes: '3',
+        tempo: 'Há 12 dias',
+        foto: 'messi.png',
+    }
+]
+
+function mostrarCards(contatos){
+
+    const container = document.getElementById('card-container')
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const img = document.createElement('img')
+    img.src = `./img/${contatos.foto}`
+    img.alt = contatos.nome
+
+    const informacoes = document.createElement('div')
+    informacoes.classList.add('info')
+
+    const nomeContato = document.createElement('div')
+    nomeContato.classList.add('nomeContato')
+
+    const nomeCtt = document.createElement('h3')
+    nomeCtt.classList.add('nomeCtt')
+    nomeCtt.textContent = contatos.nome
+
+    const qtdMensagem = document.createElement('p')
+    qtdMensagem.classList.add('notificacoes')
+    qtdMensagem.textContent = contatos.notificacoes
+
+    const mensagem = document.createElement('p')
+    mensagem.classList.add('mensagem')
+    mensagem.textContent = contatos.mensagem
+
+    nomeContato.replaceChildren(nomeCtt, qtdMensagem)
+
+    informacoes.replaceChildren(nomeContato, mensagem)
+
+    const tempo = document.createElement('p')
+    tempo.classList.add('tempo')
+    tempo.textContent = contatos.tempo
+
+    const contato = document.createElement('div')
+    contato.classList.add('contato')
+
+    const contato2 = document.createElement('p')
+    contato2.textContent = contatos.contato
+
+    contato.replaceChildren(contato2)
+
+    card.replaceChildren(img, informacoes, tempo, contato)
+
+    container.appendChild(card)
+
+}
+
+contatos.forEach(mostrarCards)
